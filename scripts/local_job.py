7b92393960d18ee4d742727e0af71205c1412fba
Valentin Rosario <valentinrosario@Valentins-MacBook-Air.local>
2026-04-17 17:41:45 +0200

    update

--- scripts/local_job.py
+++ scripts/local_job.py
@@ -7,12 +7,17 @@ from Classes.GUI import User_interface
 from Classes.mu import Attenuation
 
 
+USER_HOST = "rosariovr@carbonite"
+REMOTE_DIR = "/data/rosariovr/Painting"
+
+#Function that does the remote part 
 def run_remote_part(params):
 
     #Save parameters locally
     with open("params.pkl", "wb") as f:
         pickle.dump(params, f)
 
+    #Condensed execution: Upload, Run, Download
     os.system(f"scp params.pkl {USER_HOST}:{REMOTE_DIR}/ && "
               f"ssh {USER_HOST} 'cd {REMOTE_DIR} && PYTHONPATH={REMOTE_DIR} conda run -n Painting python scripts/remote_job.py {REMOTE_DIR}/params.pkl' && "
               f"scp {USER_HOST}:{REMOTE_DIR}/result.pkl .")
@@ -20,10 +25,10 @@ def run_remote_part(params):
     #Load result
     with open("result.pkl", "rb") as f:
         projections = pickle.load(f)
-        print(projections)
+        print(projections.shape)
     print("Simulation executed, projections received!")
 
-#Runs when user clicks "Run" in GUI
+#Function: that Runs when user clicks "Run" in GUI
 def run_logic(params):
 
     #Compute mu locally
--- scripts/remote_job.py
+++ scripts/remote_job.py
@@ -1,4 +1,4 @@
-
+import sys
 import pickle
 
 from Classes.Generator import Painting_generator
@@ -7,36 +7,33 @@ from Classes.Tomography import Tomography
 
 
 def run_remote(params):
+
+    #Test to see if we are in the remote 
     print("Running on remote GPU server...")
 
     # 1. Create Painting
     painting = Painting_generator(params['dim_x'], params['dim_y'], params['thickness'],
         params['layers_val'], params['N_spheres'],params['radius'], params['sphere_val']).paint()
 
+    print(painting)
+
     # 2. Generate projection Geometry
     proj_geom = Geometry(painting, params['SO'], params['OD'], params['n_proj'],params['geometry_type'],
         params['det_x'], params['det_y'],params['spacing_x'], params['spacing_y']).projection()
 
-    # 3. Perform projections
+    # 3. Perform projections and reconstructions 
     tomo = Tomography(painting, proj_geom, params['algorithm'])
     projections = tomo.project()
     tomo.save_projections('projections',projections)
+    #slices = tomo.reconstruct(projections)
+    #tomo.save_reconstruction('slices',slices)
 
-    #4. Perform reconstructions
-    slices = tomo.reconstruct(projections)
-    tomo.save_reconstruction('slices',slices)
-
-    # 4. Save result 
+    # 4. Save projection image  
     with open("result.pkl", "wb") as f:
         pickle.dump(projections[0], f)
-    
-    print("Remote computation done!")
 
 
-if __name__ == "__main__":
-    import sys
+with open(sys.argv[1], "rb") as f:
+    params = pickle.load(f)
 
-    with open(sys.argv[1], "rb") as f:
-        params = pickle.load(f)
-
-    run_remote(params)
+run_remote(params)
