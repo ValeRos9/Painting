0fbbc031df9969eb51d02e2ed8636759e65360b2
Valentin Rosario <valentinrosario@Valentins-MacBook-Air.local>
2026-04-17 17:07:38 +0200

    update

--- scripts/local_job.py
+++ scripts/local_job.py
@@ -1,40 +1,3 @@
-import pickle, subprocess, threading, os, time
-from Classes.GUI import User_interface 
-from Classes.mu import Attenuation
-
-USER, HOST, PATH = "rosariovr", "carbonite", "/data/rosariovr/Painting"
-CMD = f"cd {PATH} && PYTHONPATH={PATH} conda run -n Painting python scripts/remote_job.py"
-
-def run_remote(params):
-    res_file = f"res_{int(time.time())}.pkl"  # Nom unique
-    try:
-        # 1. Lance SSH : envoie params (stdin), ignore les bannières (stdout=DEVNULL)
-        cmd_ssh = ["ssh", "-T", f"{USER}@{HOST}", CMD, res_file]
-        proc = subprocess.Popen(cmd_ssh, stdin=subprocess.PIPE, stdout=subprocess.DEVNULL, stderr=subprocess.PIPE)
-        _, err = proc.communicate(input=pickle.dumps(params))
-        
-        if proc.returncode != 0: raise Exception(err.decode()[:100])
-
-        # 2. Récupère le fichier résultat et nettoie
-        subprocess.run(["scp", "-q", f"{USER}@{HOST}:{PATH}/{res_file}", "."], check=True)
-        with open(res_file, "rb") as f: data = pickle.load(f)
-        print("Succès ! Projections reçues.")
-        
-        os.remove(res_file) # Nettoie local
-    except Exception as e:
-        print(f"Échec: {e}")
-    finally:
-        # Nettoie distant (silencieux)
-        subprocess.run(["ssh", "-T", f"{USER}@{HOST}", "rm", "-f", f"{PATH}/{res_file}"], capture_output=True)
-
-def run_logic(p):
-    p['sphere_val'] = Attenuation(p['E'], p['symb']).value()
-    threading.Thread(target=run_remote, args=(p,)).start()
-
-if __name__ == "__main__":
-    User_interface(callback=run_logic).run()
-
-"""
 import numpy as np
 import pickle #new
 import subprocess #new 
@@ -98,4 +61,3 @@ def run_logic(params):
 # Start GUI
 Viewer = User_interface(callback=run_logic)
 Viewer.run()
-"""
--- scripts/remote_job.py
+++ scripts/remote_job.py
@@ -1,35 +1,4 @@
-import sys, pickle
-from Classes.Generator import Painting_generator
-from Classes.Geometry import Geometry
-from Classes.Tomography import Tomography
 
-if len(sys.argv) < 2: sys.exit("Nom fichier résultat manquant")
-res_file = sys.argv[1]
-
-# 1. Lit params depuis le flux (stdin)
-try:
-    params = pickle.loads(sys.stdin.buffer.read())
-except EOFError:
-    sys.exit("Aucune donnée reçue")
-
-# 2. Calculs
-painting = Painting_generator(params['dim_x'], params['dim_y'], params['thickness'],
-    params['layers_val'], params['N_spheres'], params['radius'], params['sphere_val']).paint()
-
-proj_geom = Geometry(painting, params['SO'], params['OD'], params['n_proj'], params['geometry_type'],
-    params['det_x'], params['det_y'], params['spacing_x'], params['spacing_y']).projection()
-
-tomo = Tomography(painting, proj_geom, params['algorithm'])
-projections = tomo.project()
-
-# Archivage (optionnel)
-tomo.save_projections('projections', projections)
-tomo.save_reconstruction('slices', tomo.reconstruct(projections))
-
-# 3. Écrit le résultat dans le fichier (pas dans stdout)
-with open(res_file, "wb") as f:
-    pickle.dump(projections[0], f)
-"""
 import pickle
 
 from Classes.Generator import Painting_generator
@@ -71,4 +40,3 @@ if __name__ == "__main__":
         params = pickle.load(f)
 
     run_remote(params)
-"""
