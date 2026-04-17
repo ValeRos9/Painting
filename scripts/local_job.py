3caa833ac67949dc5c505fce40203ddb5b9afbcb
Valentin Rosario <valentinrosario@Valentins-MacBook-Air.local>
2026-04-17 17:21:38 +0200

    update

--- scripts/local_job.py
+++ scripts/local_job.py
@@ -1,63 +1,38 @@
-import numpy as np
 import pickle #new
 import subprocess #new 
 import threading #new
+import os 
 
 from Classes.GUI import User_interface 
 from Classes.mu import Attenuation
 
 
-REMOTE_USER = "rosariovr"
-REMOTE_HOST = "carbonite"
-REMOTE_PATH = "/data/rosariovr/Painting/scripts"   # same folder where remote_job.py lives
-
-
 def run_remote_part(params):
-    # 1. Save parameters locally
+
+    #Save parameters locally
     with open("params.pkl", "wb") as f:
         pickle.dump(params, f)
 
-    # 2. Send params to remote server
+    os.system(f"scp params.pkl {USER_HOST}:{REMOTE_DIR}/ && "
+              f"ssh {USER_HOST} 'cd {REMOTE_DIR} && PYTHONPATH={REMOTE_DIR} conda run -n Painting python scripts/remote_job.py {REMOTE_DIR}/params.pkl' && "
+              f"scp {USER_HOST}:{REMOTE_DIR}/result.pkl .")
 
-    subprocess.run([
-    "scp", "params.pkl",
-    f"{REMOTE_USER}@{REMOTE_HOST}:/data/rosariovr/Painting/"
-    ])
-
-    # 3. Run remote script
-    subprocess.run([
-    "ssh", f"{REMOTE_USER}@{REMOTE_HOST}",
-    "cd /data/rosariovr/Painting && PYTHONPATH=/data/rosariovr/Painting conda run -n Painting python scripts/remote_job.py /data/rosariovr/Painting/params.pkl"
-    ])
-
-    # 4. Get results back
-    subprocess.run([
-        "scp",
-        f"{REMOTE_USER}@{REMOTE_HOST}:/data/rosariovr/Painting/result.pkl",
-        "."
-    ])
-
-    # 5. Load result
+    #Load result
     with open("result.pkl", "rb") as f:
         projections = pickle.load(f)
         print(projections)
     print("Simulation executed, projections received!")
 
-
+#Runs when user clicks "Run" in GUI
 def run_logic(params):
-    # Runs when user clicks "Run" in GUI
 
-    # 1. Compute mu locally
-    mu = Attenuation(params['E'], params['symb']).value()
-    params['sphere_val'] = mu
+    #Compute mu locally
+    params['sphere_val'] = Attenuation(params['E'], params['symb']).value()
+    print("mu:", params['sphere_val'])
 
-    print("mu:", mu)
-
-    # 2. Run remote job in background (so GUI doesn't freeze)
+    #Run remote job in background (so GUI doesn't freeze)
     thread = threading.Thread(target=run_remote_part, args=(params,))
     thread.start()
 
-
 # Start GUI
-Viewer = User_interface(callback=run_logic)
-Viewer.run()
+User_interface(callback=run_logic).run()
