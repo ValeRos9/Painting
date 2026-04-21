5aa0b6fb33a04dbf78dfafa2dedba99b92224510
Valentin Rosario <valentinrosario@Valentins-MacBook-Air.local>
2026-04-21 11:27:01 +0200

    update

--- scripts/remote_job.py
+++ scripts/remote_job.py
@@ -8,43 +8,38 @@ from Classes.Tomography import Tomography
 
 
 def run_remote(params):
-
     #Test to see if we are in the remote 
     print("Running on remote GPU server...")
 
-    print(params)
-    #Compute mu locally
+    #Set mu 
     params['sphere_val'] = Attenuation(params['E'], params['symb']).value() 
-    print("mu:", params['sphere_val'])
-    print(params)
-
-    # 1. Create Painting
-
+    #Do mu 
     mu_for_each_layer = {"oil":1,"ground":2,"wood":3}
 
+    #Create Painting
     painting = Painting_generator(params['dim_x'], params['dim_y'], params['thickness'],
         mu_for_each_layer, params['N_spheres'],params['radius'], params['sphere_val']).paint()
 
+    print(Painting.volume)
 
-    # 2. Generate projection Geometry
+
+    #Generate projection Geometry
     Geom = Geometry(painting, params['SO'], params['OD'], params['n_proj'],params['geometry_type'],
         params['det_x'], params['det_y'],params['spacing_x'], params['spacing_y'])
     proj_geom = Geom.projection()
     #tomo_visual = Geom.tomosipo_visualization()
 
-
-    # 3. Perform projections and reconstructions 
+    #Perform projections and reconstructions 
     tomo = Tomography(painting, proj_geom, params['algorithm'])
     projections = tomo.project()
     tomo.save_projections('projections',projections)
     slices = tomo.reconstruct(projections)
     tomo.save_reconstruction('slices',slices)
 
-    # 4. Save projection image  
+    #Save projection image  
     with open("result.pkl", "wb") as f:
         pickle.dump(projections[0], f)
         #pickle.dump(tomo_visual,f)
-
 
 with open(sys.argv[1], "rb") as f:
     params = pickle.load(f)
