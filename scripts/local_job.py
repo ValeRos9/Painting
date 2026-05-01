36db48ec83e33428b27e4858494d4c1cfcfefa38
V. Rosario <rosariovr@carbonite.liacs.nl>
2026-05-01 19:07:37 +0200

    update

--- scripts/local_job.py
+++ scripts/local_job.py
@@ -1,37 +1,35 @@
-import pickle 
-import subprocess  
-import os 
-from Classes.GUI import User_interface 
-from Classes.mu import Attenuation
-
+import pickle, os
+from Classes.GUI import User_interface
 
 def run_remotely(params):
-
-    #SSH and Remote directory path 
     USER_HOST = "rosariovr@carbonite"
     REMOTE_DIR = "/data/rosariovr/Painting"
     
-    with open("params.pkl", "wb") as f:
+    with open("params.pkl", "wb") as f: 
         pickle.dump(params, f)
 
-    #Upload, Run, Download (Result + SVGs)
+    # Upload, Run, Download
     os.system(f"scp params.pkl {USER_HOST}:{REMOTE_DIR}/ && "
               f"ssh {USER_HOST} 'cd {REMOTE_DIR} && PYTHONPATH={REMOTE_DIR} conda run -n tomo_env python scripts/remote_job.py {REMOTE_DIR}/params.pkl' && "
               f"scp {USER_HOST}:{REMOTE_DIR}/result.pkl . && "
               f"scp {USER_HOST}:{REMOTE_DIR}/rotation.svg . && "
               f"scp {USER_HOST}:{REMOTE_DIR}/CT.svg .")
 
-    #Load result
-    with open("result.pkl", "rb") as f:
+    # Unpack dictionary
+    with open("result.pkl", "rb") as f: 
         data = pickle.load(f)
-
-    with open("proj0000.tif", "wb") as f:
-        f.write(data)
-    print("Simulation executed, projections received!")
+    
+    with open("proj0000.tif", "wb") as f: 
+        f.write(data["tiff"])
+    if data.get("rotation_svg"): 
+        with open("rotation.svg", "wb") as f: 
+        f.write(data["rotation_svg"])
+    if data.get("ct_svg"): 
+        with open("CT.svg", "wb") as f: 
+        f.write(data["ct_svg"])
 
     os.system("open -a ImageJ proj0000.tif")
     os.system("open rotation.svg")
     os.system("open CT.svg")
 
-# Start GUI
 User_interface(target=run_remotely).run()
--- scripts/remote_job.py
+++ scripts/remote_job.py
@@ -1,57 +1,41 @@
-import sys
-import pickle
-from Classes.mu import Attenuation
+import sys, pickle
 from Classes.Generator import Painting_generator
 from CT_tomosipo import Tomo
-import numpy as np
-from scipy.ndimage import zoom
 
-######## main #######
-with open(sys.argv[1], "rb") as f:
+# Load params
+with open(sys.argv[1], "rb") as f: 
     params = pickle.load(f)
 
-#Test to see if we are in the remote 
 print("Running on remote GPU server...")
 
-#Create Painting
-painting = Painting_generator(params['E'],params['pigment'],params['dim_x'], params['dim_y'], 
-    params['layers'], params['N_spheres'],params['radius']).paint()
-print(painting.volume.shape)
+# Create Painting
+painting = Painting_generator(params['E'], params['pigment'], params['dim_x'], params['dim_y'], 
+                              params['layers'], params['N_spheres'], params['radius']).paint()
 
-#CT with tomosipo
-n_proj = params['n_proj']
-det_row = params['det_x']
-det_col = params['det_y']
-spacing_x = params['spacing_x']
-spacing_y = params['spacing_y']
-SO = params['SO']
-OD = params['OD']
-Nx = 256
-Ny = 256
-n_slices = 100
+# CT Setup
+n_proj, det_row, det_col = params['n_proj'], params['det_x'], params['det_y']
+spacing_x, spacing_y, SO, OD = params['spacing_x'], params['spacing_y'], params['SO'], params['OD']
+Nx, Ny, n_slices = 256, 256, 1
 
-#Perfom CT 
-CT_tomosipo = Tomo(painting.volume,n_proj,det_row,det_col,SO,OD,spacing_x,spacing_y,Nx,Ny,n_slices)
+# Perform CT (Triggers SVG generation inside Tomo.operator)
+CT_tomosipo = Tomo(painting.volume, n_proj, det_row, det_col, SO, OD, spacing_x, spacing_y, Nx, Ny, n_slices)
+A = CT_tomosipo.operator() 
 
-A = CT_tomosipo.operator()
-
+# Run Sim & Save
 projections = CT_tomosipo.projections(A)
-CT_tomosipo.save_projections('projections',projections)
+CT_tomosipo.save_projections('projections', projections)
 
-slices = CT_tomosipo.reconstruction(projections,A)
-CT_tomosipo.save_reconstruction('slices',slices)
-print(slices.shape)
+slices = CT_tomosipo.reconstruction(projections, A)
+CT_tomosipo.save_reconstruction('slices', slices)
 
-files_to_send = {
-    "tiff": "projections/proj0000.tif",
-    "rotation_svg": "rotation.svg",
-    "ct_svg": "CT.svg"
-}
-
+# Package & Send (Safe read: handles missing files gracefully)
 result_package = {}
-for key, path in files_to_send.items():
-    with open(path, "rb") as f:
+file_map = {"tiff": "projections/proj0000.tif", "rotation_svg": "rotation.svg", "ct_svg": "CT.svg"}
+
+for key, path in file_map.items():
+    with open(path, "rb") as f: 
         result_package[key] = f.read()
+        result_package[key] = None
 
 with open("result.pkl", "wb") as f:
     pickle.dump(result_package, f)
