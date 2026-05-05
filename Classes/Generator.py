bc601b3afd616f952d8d639d514a9f8825b8e7b8
V. Rosario <rosariovr@carbonite.liacs.nl>
2026-05-05 18:36:28 +0200

    update

--- Classes/Generator.py
+++ Classes/Generator.py
@@ -44,7 +44,7 @@ class Painting_generator:
                     # else:
                     #     mu_sphere = mu.value(self.pigment[str(nbr)])
                     centers = self.random_insert_spheres(volume[i:i+thickness,:,:], self.N_spheres, self.radius, 1) #mu_sphere
-                    print("mu_pigment",mu.value(self.pigment[str(nbr)]))
+                    #print("mu_pigment",mu.value(self.pigment[str(nbr)]))
             else:
                 volume[i:i+thickness,:,:]= 0.2 #mu.value(typex)
                 print("mu_wood",mu.value(typex))
