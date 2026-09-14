5467027127bb3d61e1ad214ebcafff51e99a78f9
Valentin Rosario <valentinrosario@Valentins-MacBook-Air.local>
2026-09-14 12:32:01 +0200

    update

--- Classes/Generator.py
+++ Classes/Generator.py
@@ -25,7 +25,7 @@ class Painting_generator:
         scale = 10000/self.L
         D = [d * scale for d in self.D]
         W,H = self.W, self.H * scale
-        size = (sum(D), W, H)
+        size = (int(sum(D)), int(W), int(H))
         print(size)
         volume = np.empty(size) 
 
